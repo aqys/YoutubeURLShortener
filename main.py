5579845d7c9b6cf39350ad8d3c885d45f6145849
aqys <141941584+aqys@users.noreply.github.com>
2024-05-21 10:56:23 +0200

    Main code (simple)

---
+++ main.py
@@ -0,0 +1,20 @@
+import re
+
+def shorten_youtube_url(youtube_url):
+    # Mmatch  video ID in the YouTube URL
+    video_id_regex = re.compile(r'v=([a-zA-Z0-9_-]{11})')
+    
+    # Search for ID using regex
+    match = video_id_regex.search(youtube_url)
+    
+    if match:
+        video_id = match.group(1)
+        short_url = f"https://youtu.be/{video_id}"
+        return short_url
+    else:
+        return "Invalid YouTube URL"
+
+
+youtube_url = input("Enter the YouTube URL: ")
+short_url = shorten_youtube_url(youtube_url)
+print(f"Shortened URL: {short_url}")
